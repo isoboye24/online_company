82d14c3e63928e2c808a19945ac05a7d90906af6
isoboye24 <isobarguy4real@gmail.com>
2025-09-11 13:02:04 +0200

    Login form customized

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Admin Dashboard</title>
+    <title>Admin Dashboard | {{ config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc." />
     <meta name="author" content="Zoyothemes" />
