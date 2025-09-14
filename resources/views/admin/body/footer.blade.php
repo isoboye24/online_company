f97db8cee01fdadea5d767ef96a9624aff122c65
isoboye24 <isobarguy4real@gmail.com>
2025-09-14 22:38:36 +0200

    Admin dashboard edited

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -4,8 +4,8 @@
             <div class="col fs-13 text-muted text-center">
                 &copy; <script>
                 document.write(new Date().getFullYear())
-                </script> - Made with <span class="mdi mdi-heart text-danger"></span> by <a href="#!"
-                    class="text-reset fw-semibold">Zoyothemes</a>
+                </script> - Made with <span class="mdi mdi-heart text-danger"></span> by <a
+                    href="https://div-mu.vercel.app/" target="_blank" class="text-reset fw-semibold">DIV</a>
             </div>
         </div>
     </div>
