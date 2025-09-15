cf1518e8d6c4b0e4b3f3773d72d287cf1ae10016
isoboye24 <isobarguy4real@gmail.com>
2025-09-15 23:46:51 +0200

    Two-Factor Authentication

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\VerificationCodeMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -19,5 +22,41 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/login');
+    }
+
+    public function adminLogin(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $verificationCode = random_int(100000, 999999);
+            session(['verification_code'=>$verificationCode, 'user_id' => $user->id]);
+
+            Mail::to($user->email)->send(new VerificationCodeMail($verificationCode));
+
+            Auth::logout();
+
+            return redirect()->route('custom.verification.form')->with('status', 'Verification code sent to your mail');
+
+            return redirect()->back()->withErrors(['email' => 'Invalid Credentials Provided']);
+        }
+    }
+
+    public function showVerification(){
+    return view('auth.verify');
+    }
+      // End Method
+
+    public function verificationVerify(Request $request){
+        $request->validate(['code' => 'required|numeric']);
+
+        if ($request->code == session('verification_code')) {
+            Auth::loginUsingId(session('user_id'));
+
+            session()->forget(['verification_code','user_id']);
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors(['code' => 'Invalid Verification Code']);
     }
 }
